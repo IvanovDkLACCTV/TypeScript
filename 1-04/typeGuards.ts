cfaadc5c70902bf4b10162150aab57f09613b6a0
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2025-07-25 15:26:58 +0300

    finished with type guards

--- 1-04/typeGuards.ts
+++ 1-04/typeGuards.ts
@@ -3,49 +3,53 @@
     var log = console.log
 
     interface User {
-    name: string
-    email: string
-    login: string
-    password: string
-  }
-
-  const user = {
-    name: "John Doe",
-    email: "johndoe@example.com",
-    login: "johndoe",
-    password: "123456",
-  } as User
-
-  interface Admin {
-    name: string
-    role: number
-  }
-
-  function logId(id: string | number) {
-    if (isString(id)) {
-        log(id)
-    } else  {
-        log(id)
+        name: string
+        email: string
+        login: string
+        password: string
     }
-    
-}
 
-function isString(x: string | number): x is string {
-    return typeof x === "string"
-}
+    const user = {
+        name: "John Doe",
+        email: "johndoe@example.com",
+        login: "johndoe",
+        password: "123456",
+    } as User
+
+    interface Admin {
+        name: string
+        role: number
+    }
+
+    function logId(id: string | number) {
+        if (isString(id)) {
+            log(id)
+        } else {
+            log(id)
+        }
+
+    }
+
+    function isString(x: string | number): x is string {
+        return typeof x === "string"
+    }
 
 
-function isAdmin(user: User | Admin): user is Admin {
-    return 'role' in user
-}
+    function isAdmin(user: User | Admin): user is Admin {
+        return 'role' in user
+    }
 
-function setRoleZero(user: User | Admin) {
-    
-}
+    function isAdminAlternative(user: User | Admin): user is Admin {
+        return (user as Admin).role !== undefined
+    }
 
-
-
-
+    function setRoleZero(user: User | Admin) {
+        if (isAdmin(user)) {
+            user.role = 0
+        } else {
+            throw new Error('User is not admin')
+        }
+    }
 
 
 })()
