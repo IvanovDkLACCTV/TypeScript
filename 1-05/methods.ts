b4dd61b4d80b4bd0b6119148cc042b1b125ac662
IvanovDkLACCTV <ivanovdklacctv@gmail.com>
2025-07-28 17:56:13 +0300

    started to learn methods of classes

--- 1-05/methods.ts
+++ 1-05/methods.ts
@@ -1,3 +1,25 @@
+enum PaymentStatus {
+    Holt,
+    Processed,
+    Reversed
+}
+
 class Payment {
     id: number
+    status: PaymentStatus
+    createdAt: Date
+    updatedAt: Date
+
+
+    constructor(id: number) {
+        this.id = id
+        this.createdAt = new Date()
+        this.status = PaymentStatus.Holt
+    }
+
+    getPaymentLifeTime(): number{
+        return new Date().getTime() - this.updatedAt.getTime()
+    }
 }
+
+const payment = new Payment(1)
